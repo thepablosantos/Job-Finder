43b341c58214972e4d3eb67a37203550cc877f44
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-14 22:22:23 +0000

    job

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // db connection
 db 
