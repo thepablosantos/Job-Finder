a60a2fdf595af171181f97a6fc51168b29d3056f
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-15 23:26:24 +0000

    job

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const { create } = require('express-handlebars');
 const path = require('path');
@@ -13,7 +14,7 @@ const PORT = 3000;
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Template Engine (Handlebars)
+// Handlebars
 const hbs = create({ defaultLayout: 'main' });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -23,45 +24,38 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // DB connection
-db
-  .authenticate()
-  .then(() => {
-    console.log("Conectou ao banco com sucesso");
-  })
-  .catch(err => {
-    console.log("Ocorreu um erro ao conectar", err);
-  });
+db.authenticate()
+  .then(() => console.log("Connected to DB successfully"))
+  .catch(err => console.log("DB connection error:", err));
 
-// Routes
+// Home route -> `home.handlebars`
 app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Jobs listing -> `jobs.handlebars`
+app.get('/jobs', (req, res) => {
   let search = req.query.job;
   let query = '%' + search + '%';
 
   if (!search) {
-    Job.findAll({
-      order: [['createdAt', 'DESC']],
-    })
-      .then((jobs) => {
-        res.render('index', { jobs });
-      })
+    Job.findAll({ order: [['createdAt', 'DESC']] })
+      .then((jobs) => res.render('jobs', { jobs }))
       .catch((err) => console.log(err));
   } else {
     Job.findAll({
-      where: {
-        title: { [Op.like]: query }
-      },
-      order: [['createdAt', 'DESC']],
+      where: { title: { [Op.like]: query } },
+      order: [['createdAt', 'DESC']]
     })
-      .then((jobs) => {
-        res.render('index', { jobs, search });
-      })
+      .then((jobs) => res.render('jobs', { jobs, search }))
       .catch((err) => console.log(err));
   }
 });
 
-// Jobs Routes
+// Sub-rotas do Jobs -> add, view, etc.
 app.use('/jobs', require('./routes/jobs'));
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
